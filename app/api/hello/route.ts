b7b10d4a55adaef23d4b35ec23d0f90c544c4676
onojaonoja2 <samuelonoja06@gmail.com>
2024-12-30 09:37:42 +0100

    added a testfetch

--- app/api/hello/route.ts
+++ app/api/hello/route.ts
@@ -1,5 +1,5 @@
 export async function GET() {
-    return new Response('Hello from a next.js route handler!', {
+    return new Response(JSON.stringify({message: 'Hello from a next.js route handler!'}), {
         status: 200,
     });
 }
