7bdd7ac0a31d7524951c98a8858676807e529a14
onojaonoja2 <samuelonoja06@gmail.com>
2024-12-30 21:14:05 +0100

    added database fetch for products

--- app/products/page.tsx
+++ app/products/page.tsx
@@ -1,11 +1,17 @@
 import ProductsList from "../ProductsList";
-import { products } from "../product-data";
 
-export default function ProductsPage() {
+
+export default async function ProductsPage() {
+    const response = await fetch('http://localhost:3000/api/products');
+    if (!response.ok) {
+        throw new Error('Network response was not ok');
+    }
+    const products = await response.json();
+
     return (
     <div className="container mx-auto p-8">
         <h1 className="text-4xl font-bold mb-8">Products</h1>
         <ProductsList products={products} />
     </div>
-    )
+    );
 }
