47f327e1a08b96fa82a3be34e8abd4e514b8fd13
onojaonoja2 <samuelonoja06@gmail.com>
2025-01-04 20:20:08 +0100

    added remove from cart functionality

--- app/ProductsList.tsx
+++ app/ProductsList.tsx
@@ -22,7 +22,26 @@ export default function ProductsList({ products, initialCartProducts = [] }: { p
     const updatedCartProducts = await response.json();
     setCartProducts(updatedCartProducts);
   }
+
+  async function removeFromCart(productId: string) {
+    const response = await fetch('http://localhost:3000/api/users/2/cart', {
+      method: 'DELETE',
+      body: JSON.stringify({
+        productId,
+      }),
+      headers: {
+        'Content-Type': 'application/json',
+      }
+    });
+    const updatedCartProducts = await response.json();
+    setCartProducts(updatedCartProducts);
+  }
+
   
+  function productIsInCart(productId: string) { 
+    return cartProducts.some(cp => cp.id === productId);
+  }
+
   return (
     <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-8">
       {products.map(product => (
@@ -41,7 +60,22 @@ export default function ProductsList({ products, initialCartProducts = [] }: { p
           </div>
           <h2 className="text-xl font-semibold mb-2">{product.name}</h2>
           <p className="text-gray-600">${product.price}</p>
-          <button onClick={() => addToCart(product.id)}>Add to Cart</button>
+          {productIsInCart(product.id)
+          ? (
+            <button 
+            className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded w-full"
+            onClick={(e) => {
+              e.preventDefault();
+              removeFromCart(product.id);
+            }}>Remove from Cart</button>
+          ) : (
+            <button 
+            className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded w-full"
+            onClick={(e) => {
+              e.preventDefault();
+              addToCart(product.id);
+            }}>Add to Cart</button>
+          )}
         </Link>
       ))}
     </div>
--- app/cart/page.tsx
+++ app/cart/page.tsx
@@ -1,7 +1,7 @@
 import ShoppingCartList from "./ShoppingCartList";
 
 export default async function CartPage() {
-  const response = await fetch('ttp://localhost:3000/api/users/2/cart', {
+  const response = await fetch('http://localhost:3000/api/users/2/cart', {
     cache: 'no-cache',
   });
   const cartProducts = await response.json();
