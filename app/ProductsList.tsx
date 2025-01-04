73979058306e591afd045ed2a9b9d8ff78708dee
onojaonoja2 <samuelonoja06@gmail.com>
2025-01-04 12:18:08 +0100

    adding product to cart

--- app/ProductsList.tsx
+++ app/ProductsList.tsx
@@ -1,18 +1,49 @@
-import Image from "next/image";
-import Link from "next/link";
+'use client';
+
+import { cache, useState } from 'react';
+import Image from 'next/image';
+import Link from 'next/link';
 import { Product } from "./product-data";
 
-export default function ProductsList({ products}: { products: Product[]}) {
-    return (
-        <div>
-            {products.map(product => (
-                <Link key={product.id} href={"/products/" + product.id}>
-                    <Image src={'/' + product.imageUrl} alt={product.name} width={150} height={150} />
-                    <h2>{product.name}</h2>
-                    <p>N{product.price}</p>
-                </Link>
-            ))}
-        </div>
-    )
-
+export default function ProductsList({ products, initialCartProducts = [] }: { products: Product[], initialCartProducts: Product[] }) {
+  const [cartProducts, setCartProducts] = useState(initialCartProducts)
+  
+  async function addToCart(productId: string) {
+    const response = await fetch('http://localhost:3000/api/users/2/cart', {
+      method: 'POST',
+      body: JSON.stringify({
+        productId,
+      }),
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      
+    });
+    const updatedCartProducts = await response.json();
+    setCartProducts(updatedCartProducts);
+  }
+  
+  return (
+    <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-8">
+      {products.map(product => (
+        <Link
+          key={product.id}
+          href={`/products/${product.id}`}
+          className="bg-white rounded-lg shadow-md p-4 hover:shadow-lg transition duration-300"
+        >
+          <div className="flex justify-center mb-4 h-48 relative"> {/* Added height and relative positioning */}
+            <Image
+              src={'/' + product.imageUrl}
+              alt="Product image"
+              fill // Fill the container
+              className="object-cover rounded-md" // Cover the container, maintaining aspect ratio
+            />
+          </div>
+          <h2 className="text-xl font-semibold mb-2">{product.name}</h2>
+          <p className="text-gray-600">${product.price}</p>
+          <button onClick={() => addToCart(product.id)}>Add to Cart</button>
+        </Link>
+      ))}
+    </div>
+  );
 }
